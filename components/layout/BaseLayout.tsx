7c2d7117c9224fc2304114c10072dfefbf8d870c
Yitong Wu <44213937+WUY97@users.noreply.github.com>
2023-01-26 14:16:53 -0800

    nft ui & display

--- components/layout/BaseLayout.tsx
+++ components/layout/BaseLayout.tsx
@@ -1,8 +1,10 @@
-import { FunctionComponent } from 'react';
-
 import Navbar from '../navbar';
 
-const BaseLayout: FunctionComponent = ({ children }) => {
+type Props = {
+    children: React.ReactNode;
+};
+
+const BaseLayout: React.FC<Props> = ({ children }) => {
     return (
         <>
             <Navbar />
