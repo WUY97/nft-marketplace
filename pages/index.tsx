cf77f9c3ec188c78ae7057086f9732aee59de36e
Yitong Wu <44213937+WUY97@users.noreply.github.com>
2023-01-26 14:58:19 -0800

    navbar & routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,8 @@ const Home: NextPage = () => {
         <BaseLayout>
             <div className='relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8'>
                 <div className='absolute inset-0'>
-                    <div className='bg-white h-1/3 sm:h-2/3' />
+                    <div className='h-1/3 sm:h-2/3' />
+                    {/* <div className='bg-white h-1/3 sm:h-2/3' /> */}
                 </div>
                 <div className='relative'>
                     <div className='text-center'>
